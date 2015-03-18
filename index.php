e4801847a17816e84e1155f9a8837039f5039af1
andrea742 <andreasabiu@hotmail.it>
2015-03-18 17:25:37 +0100

    modified:   index.php 	renamed:    php/model/contattaci.php -> php/contattaci.php 	renamed:    php/model/imperdibili.php -> php/imperdibili.php 	modified:   php/index.php 	renamed:    php/model/inevidenza.php -> php/inevidenza.php 	renamed:    php/model/login.php -> php/login.php 	renamed:    php/model/logout.php -> php/logout.php 	renamed:    php/model/ricerca.php -> php/ricerca.php 	renamed:    php/model/vendi.php -> php/vendi.php

--- index.php
+++ index.php
@@ -1,30 +1,7 @@
-<!DOCTYPE html>
-<html>
-    <head>
-    
-    </head>
-    <body>
+<?php
 
+$index = "php/index.php";
 
+header("Location:".$index);
+?>
 
-<h2>PURCHASE.IT</h2>
-<h2>Purchase.it si tratta di un sito E-Comerce dove si possono vendere qualsiasi tipo di oggetti sia nuovi che usati.</h2>
-<p>&nbsp;</p>
-<p>Requisiti soddisfatti: </p>
-<p>&nbsp;</p>
-<p>-HTML e CSS;</p>
-<p>-Model View Controller;</p>
-<p>-MySQL e PHP</p>
-<p>-Due ruoli: Seller(Venditore) e Buyer(Acquirente);</p>
-<p>Puoi andare direttamente al sito <a href="php/index.php">Cliccando qui</a></p>
-<h2><span style="font-family: Cambria, 'Hoefler Text', 'Liberation Serif', Times, 'Times New Roman', serif"></span><br>
-</h2>
-
-
-
-
-
-
-
-</body>
-</html>
--- php/index.php
+++ php/index.php
@@ -1,86 +1,73 @@
+<!DOCTYPE html>
+
 <?php
 
-include_once 'controller/ControllerBase.php';
-include_once 'controller/ControllerSeller.php';
-include_once 'controller/ControllerBuyer.php';
-
-date_default_timezone_set("Europe/Rome");
-
-FrontController::dispatch($_REQUEST);
-
-class FrontController {
-	
-	public static function dispatch(&$request) {
-		
-        session_start();
-        if (isset($request["page"])) {
-
-            switch ($request["page"]) {
-                case "login":
+setcookie("redirect", null);
+if(!isset($_COOKIE["identificazione"]))
+{
+?>
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+        <meta name="author" content="Sabiu Andrea 47736"/>
+        <meta name="description" content="Sito eCommerce"/>
+        <title>Purchase.it - Login</title>
+        <link rel="shortcut icon" type="image/x-icon" href="../immagini/favicon.png"/>
+        <link rel="Stylesheet" type="text/css" href="../css/style.css" media="screen"/>
+     </head>
+     
+     
+     <body>
+     
+     
+    <div id="paginazione">
+    <div id="logo">
+    
+    <div id="arrivo">
+                        
+                            <a href="login.php" target="_blank"><img src="../immagini/login.jpg" width="180" height="60" /></a>
+                       
+                        </div>
+                </div>
+    
+    <div id="main">
+                    <ul>
                     
-                    $controller = new BaseController();
-                    $controller->handleInput($request);
-                    break;
-					
-					
-				case 'seller':
+                        <li><a href="vendi.php" id="Vendi">Vendi</a></li>
+                        <li><a href="imperdibili.php" id="Imperdibili">Imperdibili</a></li>
+                        <li><a href="inevidenza.php" id="inevidenza">In evidenza</a></li>
+                        <li><a href="contattaci.php" id="Contattaci">Contattaci</a></li>
+                        <li><a href="ricerca.php" id="Ricerca">Ricerca</a></li>
+                    </ul>
+                </div>
+                
+                
+                
+                
+                
+                
                     
-                    $controller = new ControllerSeller();
-                    if (isset($_SESSION[ControllerBase::role]) &&
-                        $_SESSION[ControllerBase::role] != User::Seller) {
-                        self::write403();
-                    }
-                    $controller->handleInput($request);
-                    break;
-					
-				case 'buyer':
-                    
-                    $controller = new ControllerBuyer();
-                    if (isset($_SESSION[ControllerBase::role]) &&
-                        $_SESSION[ControllerBase::role] != User::Buyer) {
-                        self::write403();
-                    }
-                    $controller->handleInput($request);
-                    break;
-					
-					default:
-                    self::write404();
-                    break;
-            }
-        } else {
-            self::write404();
-        }
-    }
-	
-	
-	
-	
-	
-	
-	 public static function write404() {
-        header('HTTP/1.0 404 Not Found');
-        $titolo = "File non trovato!";
-        $messaggio = "La pagina che hai richiesto non &egrave; disponibile";
-        include_once('error.php');
-        exit();
-    }
-	
-	
-	
-	
-	
-	
-	    public static function write403() {
-        header('HTTP/1.0 403 Forbidden');
-        $titolo = "Accesso negato";
-        $messaggio = "Non hai i diritti per accedere a questa pagina";
-        $login = true;
-        include_once('error.php');
-        exit();
-    }
 
+      
+     
+     
+     
+     </body>
+     </html>
+     
+     
+<?
 }
 
+else
+{
+    $login = "logout.php";
+    $redirect = "inizio.php";
+
+    setcookie("redirect", $redirect, time()+300);
+
+	header("Location:".$login);
+}
 ?>
 					
 				
