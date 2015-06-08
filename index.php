8d7745435cbe80b503dfabb2f876a75f97d8e4e9
andrea742 <andreasabiu@hotmail.it>
2015-06-09 00:27:34 +0200

    modified:   index.php

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
 
-$index = "php/index.php";
+<h1>Benvenuto su Purchase.it </h1><br><br>
+<h2> Progetto AMM di Sabiu Andrea </h2>
+<p> Si tratta di un sito e-commerce che permette l'acquisto di un qualsiasi tipo di oggetto in maniera facile, gratuita e veloce.</p> <br><br>
 
-header("Location:".$index);
-?>
+<p> Esistono 2 tipi di utenti: 
 
+<ul>
+<li> Cliente</li>
+<li> Venditore </li>
+
+</ul>
+
+
+
+</body>
+</html>
