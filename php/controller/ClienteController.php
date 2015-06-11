145ac7bae16e1993b1433e0d63c8b44523d797de
andrea742 <andreasabiu@hotmail.it>
2015-06-11 15:40:00 +0200

    modified:   php/controller/ClienteController.php 	new file:   php/model/Articolo_ordineFactory.php 	new file:   php/model/OrdineFactory.php

--- php/controller/ClienteController.php
+++ php/controller/ClienteController.php
@@ -1,10 +1,8 @@
 <?php
 
 include_once 'BaseController.php';
-include_once basename(__DIR__) . '/../model/Pizza_ordineFactory.php'; //controlla questo
-include_once basename(__DIR__) . '/../model/OrarioFactory.php';//controlla questo
-include_once basename(__DIR__) . '/../model/PizzaFactory.php';//controlla questo
-include_once basename(__DIR__) . '/../model/OrdineFactory.php';//controlla questo
+include_once basename(__DIR__) . '/../model/OrdineFactory.php';
+include_once basename(__DIR__) . '/../model/Articolo_ordineFactory.php';//controlla questo
 
 class ClienteController extends BaseController { // questa funzione è da modificare
 
@@ -91,10 +89,10 @@ class ClienteController extends BaseController { // questa funzione è da modifi
                         $vd->setSottoPagina('conferma_ordine');
                         $msg = array();
                      
-                        $idPizze = PizzaFactory::instance()->getIdPizze();
+                        $idOrdine = OrdineFactory::instance()->getIdOrdine();
                         
                        
-                        $nPizze = $this->validaForm($idPizze, $request);
+                        $nOrdine = $this->validaForm($idOrdine, $request);
                         $flagOrario = false;
                         
                        
@@ -103,47 +101,11 @@ class ClienteController extends BaseController { // questa funzione è da modifi
                         $ordineId = $ordine->getId();
                         
                       
-                        if($nPizze){
-                                
-                            $orari = OrarioFactory::instance()->getOrariSuccessivi($request['orario']);  
-                            foreach ($orari as $orario) {
-                                if((Pizza_ordineFactory::instance()->getNPizzePerOrario($orario->getId())+$nPizze) <= $orario->getOrdiniDisponibili()){
-                                    
-                                    $ordine->setOrario($orario->getId());
-                                    $flagOrario = true;
-                                    break;
-                                }else $ordine->setOrario(NULL);
-                            }
-                        }
-                        else{
-                            $msg[]='<li>I valori inseriti non sono validi. Ordine annullato</li>';
-                            $vd->setSottoPagina('ordina');    
-                            $this->creaFeedbackUtente($msg, $vd, "");
-                            $this->showHomeUtente($vd);
-                        break;                            
-                        }
                         
-						if($flagOrario){
-                            
-                            OrdineFactory::instance()->nuovoOrdine($ordine);     //controlla                      
-
-                            foreach($idPizze as $idPizza){
-                                $quantita = filter_var($request[$idPizza.'normali'], FILTER_VALIDATE_INT,  FILTER_NULL_ON_FAILURE);
-                                if (isset($quantita)){
-                                   Pizza_ordineFactory::instance()->creaPO($idPizza, $ordineId, $quantita, "normale");}
-                                $quantita = filter_var($request[$idPizza.'giganti'], FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILURE);    
-                                if (isset($quantita)){
-                                   Pizza_ordineFactory::instance()->creaPO($idPizza, $ordineId, $quantita, "gigante");}
-                            }
-                            OrdineFactory::instance()->aggiornaOrdine($user, $ordine, $request['domicilio']);                     
-                        } 
+                        
+						
                 
-                        else {
-                            Pizza_ordineFactory::instance()->cancellaPO($ordineId);
-                            OrdineFactory::instance()->cancellaOrdine($ordineId);                           
-                            $msg[]= '<li>Non è possibile ordinare questo quantitativo di pizze in nessuna fascia oraria odierna';
-                            $vd->setSottoPagina('ordina');                            
-                        }
+                       
                         $this->creaFeedbackUtente($msg, $vd, "");
                         $this->showHomeUtente($vd);
                         break;
@@ -156,7 +118,7 @@ class ClienteController extends BaseController { // questa funzione è da modifi
                         $_SESSION['pagina'] = 'dettaglio_ordine.php'; 
                         $ordineId = filter_var($request['ordine'], FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILURE);
                         $ordine = OrdineFactory::instance()->getOrdine($ordineId);
-                        $POs = Pizza_ordineFactory::instance()->getPOPerIdOrdine($ordine);
+                        $POs = Articolo_ordineFactory::instance()->getPOPerIdOrdine($ordine);
                         $vd->setSottoPagina('dettaglio_ordine');
                         $this->showHomeUtente($vd);
                         break; 
@@ -176,21 +138,7 @@ class ClienteController extends BaseController { // questa funzione è da modifi
 					//controlla questo caso 
 					
 					
-                    case 'cancella_ordine':
-                      
-                        $msg = array();
-                        $ordineId = $request['ordineId'];
-                        $p = Pizza_ordineFactory::instance()->cancellaPO($ordineId);
-                        $o = OrdineFactory::instance()->cancellaOrdine($ordineId);
-                        if ($p && $o) {
-                            $this->creaFeedbackUtente($msg, $vd, "Ordine ".$ordineId." cancellato.");
-                        }else $this->creaFeedbackUtente('<li>Errore cancellazione</li>', $vd, "");
-                        $vd->setSottoPagina('home');
-                        $this->showHomeUtente($vd);
-                        break;
-                        
                     
-					//controlla questo caso 
 					
                     case 'indirizzo':
                      
@@ -230,18 +178,7 @@ class ClienteController extends BaseController { // questa funzione è da modifi
 		
 		 }
  
-   //questa funzione è da modificare
-    private function validaForm($idPizze , $request) {
-         $valide = 0;
-         foreach($idPizze as $idPizza){
-            $quantitaN = filter_var($request[$idPizza.'normali'], FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILURE);
-            if (isset($quantitaN) && ($quantitaN != 0)) $valide+=$quantitaN;
-            $quantitaG = filter_var($request[$idPizza.'giganti'], FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILURE);
-            if (isset($quantitaG) && ($quantitaG != 0)) $valide+=$quantitaG;   
-         }
-         
-         return $valide;
-    }
+  
 
 }
 
