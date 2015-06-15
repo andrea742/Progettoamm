4cf171e77fa9e9e15a2449dfdf5653841609c42d
andrea742 <andreasabiu@hotmail.it>
2015-06-15 22:03:48 +0200

    modified:   php/view/cliente/menu.php 	modified:   php/view/venditore/logo.php 	modified:   php/view/venditore/menu.php

--- php/view/cliente/menu.php
+++ php/view/cliente/menu.php
@@ -1,9 +1,6 @@
 <ul id="menuTop">
     <li class="<?= strpos($vd->getSottoPagina(),'home') !== false || $vd->getSottoPagina() == null ? 'current_page_item' : ''?>"><a href="cliente/home">Home</a></li>
-    <li class="riga"></li>
     <li class="<?= strpos($vd->getSottoPagina(),'i_miei_dati') !== false ? 'current_page_item' : '' ?>"><a href="cliente/i_miei_dati">I miei dati</a></li>
-    <li class="riga"></li>
     <li class="<?= strpos($vd->getSottoPagina(),'carrello') !== false ? 'current_page_item' : '' ?>"><a href="cliente/carrello">Il mio carrello</a></li>
-    <li class="riga"></li>
     <li class="<?= strpos($vd->getSottoPagina(),'contatti') !== false ? 'current_page_item' : '' ?>"><a href="cliente/contatti">Contatti</a></li>
 </ul>
