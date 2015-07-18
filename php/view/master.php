07f68dfd5ebfa7e8d2010a0d0cb0f4b1abbdf6aa
andrea742 <andreasabiu@hotmail.it>
2015-07-18 18:49:20 +0200

    modified:   php/view/master.php

--- php/view/master.php
+++ php/view/master.php
@@ -93,19 +93,7 @@ if (!$vd->isJson()) {
 
             </div>
                             <!--  footer -->
-            <footer>
-                <div id="footer">
-                    <p>
-                                              
-                        <br>                      
-                        <a href="http://validator.w3.org/check/referer" class="xhtml" title="Questa pagina contiene HTML valido">
-                            <abbr title="eXtensible HyperText Markup Language">HTML</abbr> Valido</a>
-                        <a href="http://jigsaw.w3.org/css-validator/check/referer" class="css" title="Questa pagina ha CSS validi">
-                            <abbr title="Cascading Style Sheets">CSS</abbr> Valido</a>
-                    </p>
-                </div>
-
-            </footer>
+            
         </body>
     </html>
     <?php
