311e5e1ea457e35f2ca27d39eeb845172d4a150d
andrea742 <andreasabiu@hotmail.it>
2015-07-18 14:48:48 +0200

    modified:   php/view/cliente/logo.php 	modified:   php/view/venditore/logo.php

--- php/view/cliente/logo.php
+++ php/view/cliente/logo.php
@@ -1,5 +1,5 @@
 <div id ="logout">
-    <p>BENVENUTO <?= $user->getNome().' '.$user->getCognome() Non sei tu? ?></p>
+    <p>BENVENUTO <?= $user->getNome().' '.$user->getCognome() ?> Non sei tu? </p>
     <p class="logout">
         <a href="cliente?cmd=logout">Logout</a>
     </p>
