c8f2989e43c44badd78dcb5857cb9379cfd9c537
andrea742 <andreasabiu@hotmail.it>
2015-07-19 21:20:32 +0200

    aa

--- php/cliente/home.php
+++ php/cliente/home.php
@@ -37,14 +37,14 @@
 		</div>
 
                 </header>
-       <h2>prova scrittura</h2>
+       <h2>Ruolo Cliente</h2>
        
        <h3>  
        Questa pagina &egrave la pagina dedicata al ruolo cliente, da qua potrai fare acquisti inserendo nuovi articoli
        all'interno del tuo carrello e qualora non sia convinto di volerlo acquistare potrai rimuoverlo in qualsiasi
-       momento grazie alla funzione "Rimuovi dal carrello". <br><br> Non trovi l'articolo che stai cercando nella sezione 
+       momento grazie alla funzione "Rimuovi dal carrello". <br><br><br><br> Non trovi l'articolo che stai cercando nella sezione 
        "Oggetti in vendita"? <br><br> Nessun problema, utilizza la funzione cerca per vedere se il venditore mette a disposizione
-       l'articolo. <br><br> Quanto termini la sessione non scordarti di utilizzare il tasto Logout per terminare la tua sessione.
+       l'articolo. <br><br><br><br> Quanto termini la sessione non scordarti di utilizzare il tasto Logout per terminare la tua sessione.
        
        
        </h3>
