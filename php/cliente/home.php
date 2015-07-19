461ec09f50b113818f4e9961a91ea1744bf4afc1
andrea742 <andreasabiu@hotmail.it>
2015-07-19 21:16:58 +0200

    aa

--- php/cliente/home.php
+++ php/cliente/home.php
@@ -37,7 +37,17 @@
 		</div>
 
                 </header>
-
+       <h3>prova scrittura</h3>
+       
+       <h2>  
+       Questa pagina è la pagina dedicata al ruolo cliente, da qua potrai fare acquisti inserendo nuovi articoli
+       all'interno del tuo carrello e qualora non sia convinto di volerlo acquistare potrai rimuoverlo in qualsiasi
+       momento grazie alla funzione "Rimuovi dal carrello". <br> Non trovi l'articolo che stai cercando nella sezione 
+       "Oggetti in vendita"? <br> Nessun problema, utilizza la funzione cerca per vedere se il venditore mette a disposizione
+       l'articolo.
+       
+       
+       </h2>
                 
                 
 	<br>
