ba3f9e44783c7dca2a4db4024f957df2ea862d0f
andrea742 <andreasabiu@hotmail.it>
2015-07-19 12:59:35 +0200

    aa

--- php/view/venditore/menu.php
+++
@@ -1,6 +0,0 @@
-<ul id="menuTop">
-    <li class="<?= strpos($vd->getSottoPagina(),'home') !== false || $vd->getSottoPagina() == null ? 'current_page_item' : ''?>"><a href="cliente/home">Home</a></li>
-    <li class="<?= strpos($vd->getSottoPagina(),'i_miei_dati') !== false ? 'current_page_item' : '' ?>"><a href="cliente/i_miei_dati">I miei dati</a></li>  
-    <li class="<?= strpos($vd->getSottoPagina(),'le_mie_vendite') !== false ? 'current_page_item' : '' ?>"><a href="cliente/le_mie_vendite">Le mie vendite</a></li>
-    <li class="<?= strpos($vd->getSottoPagina(),'metti_in_vendita') !== false ? 'current_page_item' : '' ?>"><a href="cliente/metti_in_vendita">Metti in vendita</a></li>   
-</ul>
