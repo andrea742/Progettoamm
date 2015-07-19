befda9416e95eecd560fa32984ab352143448542
andrea742 <andreasabiu@hotmail.it>
2015-07-19 13:19:18 +0200

    aa

--- php/cliente/ricerca.php
+++ php/cliente/ricerca.php
@@ -39,13 +39,13 @@
                 </header>
                 
                 <?php
-                $connessione_al_server = mysql_connect("localhost","mameliSimone","macaco861");
+                $connessione_al_server = mysql_connect("localhost","sabiuAndrea","talpa816");
                 
                 if(!$connessione_al_server)
                 {
                 	die("Errore: connessione non riuscita".mysql_error());
             	}
-            	$db_selected = mysql_select_db("amm14_mameliSimone", $connessione_al_server);
+            	$db_selected = mysql_select_db("amm14_sabiuAndrea", $connessione_al_server);
             	if(!$db_selected)
             	{
                 	die("Errore: selezione del database errata ".mysql_error());
